b75d696475c11d9165d48a310ff29368ed6c62ea
Allan <allan.assense@hotmail.fr>
2018-04-11 17:03:01 +0200

    Bibliotheque classe

--- appli/src/main/java/org/appli/Bibliotheque.java
+++ appli/src/main/java/org/appli/Bibliotheque.java
@@ -1,5 +1,26 @@
 package org.appli;
 
-public class Bibliotheque {
+import java.util.ArrayList;
+import java.util.Date;
 
+public class Bibliotheque {
+	private ArrayList<Livre> livres;
+	private ArrayList<Emprunt> emprunts;
+
+	public Bibliotheque() {
+		ArrayList<Livre> livres = new ArrayList<Livre>();
+		ArrayList<Emprunt> emprunts = new ArrayList<Emprunt>();
+	}
+
+	public void addLivre(String titre, int annee, String auteur, String editeur) {
+	}
+
+	public void addEmprunt(int id, int livre_id, String usager, Date date_emprunt) {
+	}
+
+	public void update() {
+	}
+
+	public void init() {
+	}
 }
