71a6c139ad6aa5d0347b2154f7f3198733e61440
Allan <allan.assense@hotmail.fr>
2018-04-11 14:36:39 +0200

    classe livre

--- appli/src/main/java/org/appli/Livre.java
+++ appli/src/main/java/org/appli/Livre.java
@@ -1,5 +1,53 @@
 package org.appli;
 
 public class Livre {
+	private int id;
+	private String titre;
+	private int annee;
+	private String auteur;
+	private String editeur;
+
+	public Livre(int id, String titre, int annee, String auteur, String editeur) {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitre() {
+		return titre;
+	}
+
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+
+	public int getAnnee() {
+		return annee;
+	}
+
+	public void setAnnee(int annee) {
+		this.annee = annee;
+	}
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+	public String getEditeur() {
+		return editeur;
+	}
+
+	public void setEditeur(String editeur) {
+		this.editeur = editeur;
+	}
 
 }
